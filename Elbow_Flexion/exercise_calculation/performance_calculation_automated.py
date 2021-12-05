47b5977c0e52d2191e71757f0e1d20e23efd1af9
karamalnashi <alnashi.karam123@gmail.com>
2021-12-05 17:18:43 +0100

    file json

--- Elbow_Flexion/exercise_calculation/performance_calculation_automated.py
+++ Elbow_Flexion/exercise_calculation/performance_calculation_automated.py
@@ -1,11 +1,7 @@
-
-# coding: utf-8
-#
-
-# In[3]:
-
+import json
 
 import cv2
+import paho.mqtt.client as mqttclient
 import numpy as np
 import time
 import Pose_Module as pm
@@ -41,9 +37,41 @@ x=0
 z=1
  
 
+broker_address = "l51d159d-internet-facing-6e9635f0905aed75.elb.us-east-1.amazonaws.com"
+port = 1883
+user = "test"
+password = "test"
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("client is connected")
+        global connected
+        connected = True
+    else:
+        print("client is error")
+
+
+def on_message(client, userdata, message):
+    print("message recieved = " + str(message.payload.decode("utf-8")))
+    print("message topic=", message.topic)
+
+
+Messagerecieved = False
+connected = False
+
+client = mqttclient.Client("MQTT")
+client.on_message = on_message
+client.username_pw_set(user, password=password)
+client.on_connect = on_connect
+client.connect(broker_address, port=port)
+
+client.loop_start()
+client.subscribe("ebrain/#")
+
 while True:
 	success, img = cap.read()
-	img = cv2.resize(img, (1780, 1120))
+	img = cv2.resize(img, (1280, 720))
 	# img = cv2.imread("AiTrainer/test.jpg")
 	img = detector.findPose(img, False)
 	lmList = detector.findPosition(img, False)
@@ -136,22 +164,42 @@ while True:
 									cv2.FONT_HERSHEY_PLAIN, 3,
 									(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[0]
+						y0 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y0)
 					elif angle<65:
 						# img = cv2.applyColorMap(img, cv2.COLORMAP_DEEPGREEN)
 						cv2.putText(img, str("Bitte bewege deine rechte Hand nach oben"), (20, 50),
 									cv2.FONT_HERSHEY_PLAIN, 3,
 									(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[1]
+						y1 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y1)
 					elif 65<angle<70:
 						# img = cv2.applyColorMap(img, cv2.COLORMAP_DEEPGREEN)
 						cv2.putText(img, str("und noch etwas weiter"), (20, 50),
 									cv2.FONT_HERSHEY_PLAIN, 3,
 									(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[2]
+						y2 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y2)
 					else:
 						cv2.putText(img, str("einen Moment da bleiben und dabei entspannen"), (20, 50), cv2.FONT_HERSHEY_PLAIN,
 									3,(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[3]
+						y3 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y3)
 						z=0
 		# -------------------------------------------------------------------
 		# Left Arm
@@ -162,27 +210,52 @@ while True:
 								cv2.FONT_HERSHEY_PLAIN, 3,
 								(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[4]
+						y4 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y4)
 					elif angle1 < 65 :
 							# img = cv2.applyColorMap(img, cv2.COLORMAP_DEEPGREEN)
 						cv2.putText(img, str("Bitte bewege deine linke Hand nach oben"), (20, 50),
 									cv2.FONT_HERSHEY_PLAIN, 3,
 									(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[5]
+						y5 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y5)
 					elif 65<angle1<70:
 						# img = cv2.applyColorMap(img, cv2.COLORMAP_DEEPGREEN)
 						cv2.putText(img, str("und noch etwas weiter"), (20, 50),
 									cv2.FONT_HERSHEY_PLAIN, 3,
 									(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[6]
+						y6 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y6)
 					else:
 						cv2.putText(img, str("noch einen Moment da bleiben und dabei entspannen"), (20, 50), cv2.FONT_HERSHEY_PLAIN,
 									3,(255, 0, 0), 3)
 						cv2.imshow("Image", img)
+						f = open('data.json')
+						data = json.load(f)
+						x = data[7]
+						y7 = json.dumps(x)
+						client.publish("ebrain/DialogEngine1/interaction", y7)
 						z=0
 			elif z==1:
 					cv2.putText(img, str("Nur eine Hand bewegen"), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
 								(255, 0, 0), 3)
 					cv2.imshow("Image", img)
+					f = open('data.json')
+					data = json.load(f)
+					x = data[8]
+					y8 = json.dumps(x)
+					client.publish("ebrain/DialogEngine1/interaction", y8)
 					z=1
 				#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+
 
@@ -197,12 +270,22 @@ while True:
 								cv2.FONT_HERSHEY_PLAIN, 3,
 								(255, 0, 0), 3)
 					cv2.imshow("Image", img)
+					f = open('data.json')
+					data = json.load(f)
+					x = data[9]
+					y9 = json.dumps(x)
+					client.publish("ebrain/DialogEngine1/interaction", y9)
 				elif dba-5< angle < dba:
 					# img = cv2.applyColorMap(img, cv2.COLORMAP_DEEPGREEN)
 					cv2.putText(img, str("und noch etwas weiter"), (20, 50),
 								cv2.FONT_HERSHEY_PLAIN, 3,
 								(255, 0, 0), 3)
 					cv2.imshow("Image", img)
+					f = open('data.json')
+					data = json.load(f)
+					x = data[10]
+					y10 = json.dumps(x)
+					client.publish("ebrain/DialogEngine1/interaction", y10)
 					y = 0
 					x=0
 
@@ -215,12 +298,22 @@ while True:
 
 										cv2.putText(img, str("Gut,und noch etwas weiter, wenn es geht"), (20, 50),cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 0), 3)
 										cv2.putText(img,str(j//10),(250,250), cv2.FONT_HERSHEY_PLAIN, 15,(255,255,255),10,cv2.LINE_AA)
+										f = open('data.json')
+										data = json.load(f)
+										x = data[11]
+										y11= json.dumps(x)
+										client.publish("ebrain/DialogEngine1/interaction", y11)
 
 								elif j<=9:
 
 										cv2.putText(img, str("Ihr Helfer darf jetzt den Rest ergaenzen"),  (20, 50),cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 0), 3)
 								cv2.imshow("Image", img)
 								cv2.waitKey(125)
+								f = open('data.json')
+								data = json.load(f)
+								x = data[12]
+								y12 = json.dumps(x)
+								client.publish("ebrain/DialogEngine1/interaction", y12)
 								j = j - 1
 								if j ==0:
 									x = 1
@@ -235,6 +328,11 @@ while True:
 								cv2.FONT_HERSHEY_PLAIN, 3,
 								(255, 0, 0), 3)
 					cv2.imshow("Image", img)
+					f = open('data.json')
+					data = json.load(f)
+					x = data[13]
+					y13 = json.dumps(x)
+					client.publish("ebrain/DialogEngine1/interaction", y13)
 				elif dba-5< angle1 < dba:
 					# img = cv2.applyColorMap(img, cv2.COLORMAP_DEEPGREEN)
 					cv2.putText(img, str("und noch etwas weiter"), (20, 50),
@@ -242,6 +340,11 @@ while True:
 								(255, 0, 0), 3)
 					x = 0
 					cv2.imshow("Image", img)
+					f = open('data.json')
+					data = json.load(f)
+					x = data[14]
+					y14 = json.dumps(x)
+					client.publish("ebrain/DialogEngine1/interaction", y14)
 				elif dba < angle1 < dba + 5:
 					j = 30
 					while j >= 0 and x == 0:
@@ -254,12 +357,22 @@ while True:
 									(255, 0, 0), 3)
 							cv2.putText(img, str(j // 10), (250, 250), cv2.FONT_HERSHEY_PLAIN, 15, (255, 255, 255), 10,
 										cv2.LINE_AA)
+							f = open('data.json')
+							data = json.load(f)
+							x = data[15]
+							y15 = json.dumps(x)
+							client.publish("ebrain/vDialogEngine1/interaction", y15)
 
 						elif j <= 9:
 
 							cv2.putText(img, str("Ihr Helfer darf jetzt den Rest ergaenzen"), (20, 50),
 									cv2.FONT_HERSHEY_PLAIN, 3,
 									(255, 0, 0), 3)
+							f = open('data.json')
+							data = json.load(f)
+							x = data[16]
+							y16 = json.dumps(x)
+							client.publish("ebrain/DialogEngine1/interaction", y16)
 						cv2.imshow("Image", img)
 						cv2.waitKey(125)
 
@@ -272,6 +385,11 @@ while True:
 					cv2.putText(img, str("Nur eine Hand bewegen"), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
 								(255, 0, 0), 3)
 					cv2.imshow("Image", img)
+					f = open('data.json')
+					data = json.load(f)
+					x = data[17]
+					y17 = json.dumps(x)
+					client.publish("ebrain/DialogEngine1/interaction", y17)
 					z==1
 
 ###############################################################################
@@ -314,6 +432,11 @@ while True:
 			cv2.putText(img, str("please correct posture"), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,
 					(255, 0, 0), 5)
 			cv2.imshow("Image", img)
+			f = open('data.json')
+			#data = json.load(f)
+			#x = data[18]
+			#y18 = json.dumps(x)
+			#client.publish("ebrain/DialogEngine1/interaction", y18)
 			print(difference)
 
 		else:
